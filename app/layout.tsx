04eb060e2976777690a9bb714d61e98956533609
Li Shaowen <litony0426@gmail.com>
2024-04-15 11:46:28 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Providers from "./providers";
+import Head from "next/head";
 
 import "./globals.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html className="h-full" lang="en" suppressHydrationWarning>
-      <head />
+      <Head>
+        <title>distance</title>
+      </Head>
       <body
         className="bg-background h-full  m-0 flex flex-col justify-start"
         suppressHydrationWarning
